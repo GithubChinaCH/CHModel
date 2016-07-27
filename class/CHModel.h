fa9233f13613bd3a69cc02dd7eadbbdf9f9a24ad
jhtxch <jhtxchenhao@163.com>
2016-07-27 21:47:37 +0800

    0.2.0

--- class/CHModel.h
+++ class/CHModel.h
@@ -9,21 +9,27 @@
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 #import <objc/message.h>
+#import "AFNetworking.h"
 
+typedef void(^cmpBlock)(BOOL isSuccess);
 @interface CHModel : NSObject
 
 //property
-@property(nonatomic, strong) NSMutableDictionary *parameters;
-@property(nonatomic, copy) NSString *baseUrl;
-
+@property (nonatomic, strong)        AFHTTPSessionManager *sessionManager;
+@property (nonatomic, strong)        NSMutableDictionary *parameters;
+@property (nonatomic, copy,readonly) NSString *errorStr;
+@property (nonatomic, copy)          NSString *parsingStr;
 
 //--------------method----------
 //instance
 - (instancetype)initWithJson:(id)object;
 
 //fetch
-- (void)GET:(NSString *)url CompletionBlcok:(void(^)(BOOL isSuccess, NSError *error))compBlock;
-- (void)POST:(NSString *)url CompletionBlcok:(void(^)(BOOL isSuccess, NSError *error))compBlock;
+- (void)GET:(NSString *)url CompletionBlcok:(cmpBlock)compBlock;
+- (void)POST:(NSString *)url CompletionBlcok:(cmpBlock)compBlock;
+
+//other
+- (NSDictionary *)setMapperDic;
 
 
 @end
--- class/CHModel.m
+++ class/CHModel.m
@@ -5,319 +5,269 @@
 //  Created by jhtxch on 16/2/1.
 //  Copyright © 2016年 jhtxch. All rights reserved.
 //
-
 #import "CHModel.h"
-#import "AFNetworking.h"
 
 @interface CHModel ()
-@property(nonatomic, strong) AFHTTPSessionManager *sessionManager;
-@property(nonatomic, strong) NSDictionary *mapper;//映射字典
+
+@property (nonatomic, strong) NSDictionary *mapper;//映射字典
+
 @end
 
-static const char *propertyAryKey;
-static const char *propertyDicKey;
+#define typeCode     @"typeCode"
+#define className    @"className"
+#define protocolName @"protocolName"
+
+static NSString *propertyDicKey;
+static NSString *propertyAryKey;
 
 @implementation CHModel
 
-#pragma mark - HTTPRequest
+#pragma mark - init
 
-- (void)setBaseUrl:(NSString *)baseUrl
-{
-    if (_baseUrl != baseUrl) {
-        _baseUrl = baseUrl;
-        _sessionManager = [[AFHTTPSessionManager alloc] initWithBaseURL:[NSURL URLWithString:baseUrl]];
-        [self settingDefaultHttpRequest];
-    }
-}
-
-- (AFHTTPSessionManager *)sessionManager
-{
-    if (!_sessionManager) {
-        _sessionManager = [AFHTTPSessionManager manager];
-        [self settingDefaultHttpRequest];
-    }
-    return _sessionManager;
-}
-
-- (void)settingDefaultHttpRequest
-{
-    //do some default setting
-    [_sessionManager.requestSerializer setValue:@"application/json"forHTTPHeaderField:@"Accept"];
-    [_sessionManager.requestSerializer setValue:@"application/json;charset=utf-8"forHTTPHeaderField:@"Content-Type"];
-//    [_sessionManager.requestSerializer setValue:@"8f8418050c74865be891e1dcb66ec3f2" forHTTPHeaderField:@"apikey"];
-}
-
-
-
-- (void)POST:(NSString *)url CompletionBlcok:(void (^)(BOOL, NSError *error))compBlock
-{
-    [self.sessionManager POST:url parameters:self.parameters progress:^(NSProgress * _Nonnull uploadProgress) {
-        
-    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-        NSLog(@"request success");
-        NSLog(@"%@",responseObject);
-        id value = [responseObject objectForKey:@"retData"];
-        [self analyticalObjectData:value];
-        compBlock(YES, nil);
-    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-        NSLog(@"request fail");
-        NSLog(@"error:%@",error);
-        compBlock(NO, error);
-    }];
-}
-
-- (void)GET:(NSString *)url CompletionBlcok:(void (^)(BOOL, NSError *))compBlock
-{
-    [self.sessionManager GET:url parameters:self.parameters progress:^(NSProgress * _Nonnull downloadProgress) {
-        
-    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-        NSLog(@"request success");
-        NSLog(@"%@",responseObject);
-        id value = [responseObject objectForKey:@"retData"];
-        [self analyticalObjectData:value];
-        compBlock(YES, nil);
-    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-        NSLog(@"request fail");
-        NSLog(@"error:%@",error);
-        compBlock(NO, error);
-    }];
-}
-
-
-
-#pragma mark - instance
-- (id)init
+- (instancetype)init
 {
     if (self = [super init]) {
-        [self initDefault];
+        [self initDefalt];
     }
     return self;
 }
 
-- (id)initWithJson:(id)object
+- (instancetype)initWithJson:(id)object
 {
-    if (self = [super init]) {
-        [self initDefault];
-        [self analyticalObjectData:object];
+    self = [super init];
+    if (self) {
+        [self initDefalt];
+        [self analyzeData:object];
     }
     return self;
 }
 
+#pragma mark - init default
 
-#pragma mark - initDefault
-- (void)initDefault
+- (void)initDefalt
 {
-    [self setMapperDictionary];
-    [self setPropertyAry];
+    self.parsingStr = nil;
+    [self setMapperDic];
+    [self setProperty];
 }
 
+- (void)setProperty
+{
+    if (objc_getAssociatedObject(self, &propertyDicKey) == nil || objc_getAssociatedObject(self, &propertyAryKey) == nil) {
+        unsigned int count = 0;
+        objc_property_t *propertys = class_copyPropertyList(self.class, &count);
+        NSMutableDictionary *propertyDic = [NSMutableDictionary dictionary];
+        NSMutableArray *propertyAry = [NSMutableArray array];
+        for (int i = 0; i < count ; i ++) {
+            NSString *attribute = [NSString stringWithUTF8String:property_getAttributes(propertys[i])];
+            NSString *name = [NSString stringWithUTF8String:property_getName(propertys[i])];
+            [propertyAry addObject:name];
+            [propertyDic setValue:attribute forKey:name];
+        }
+        if (count > 0) {
+            objc_setAssociatedObject(self, &propertyAryKey, propertyAry, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+            objc_setAssociatedObject(self, &propertyDicKey, propertyDic, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        }
+        free(propertys);
+    }
+}
+
+- (void)setMapperDictionary
+{
+    self.mapper = [self setMapperDic];
+}
 
 - (NSDictionary *)setMapperDic
 {
     return @{};
 }
 
-- (void)setMapperDictionary
+#pragma mark - network
+- (AFHTTPSessionManager *)sessionManager
 {
-    if (_mapper == nil) {
-        _mapper = [self setMapperDic];
+    if (!_sessionManager) {
+        _sessionManager = [AFHTTPSessionManager manager];
+        [self configurationSessionManager];
     }
+    return _sessionManager;
 }
 
-- (void)setPropertyAry
+- (void)configurationSessionManager
 {
-    if (objc_getAssociatedObject(self, &propertyAryKey) == nil || objc_getAssociatedObject(self, &propertyDicKey) == nil) {
-        unsigned int index = 0;
-        objc_property_t *property = class_copyPropertyList(self.class, &index);
-        NSMutableArray *propertyAry = [NSMutableArray array];
-        NSMutableDictionary *propertyDic = [NSMutableDictionary dictionary];
-        for (int i = 0; i < index; i ++) {
-            NSString *name = [NSString stringWithUTF8String:property_getName(property[i])];
-            NSString *attribute = [NSString stringWithUTF8String:property_getAttributes(property[i])];
-            [propertyAry addObject:name];
-            [propertyDic setObject:attribute forKey:name];
-        }
-        if (index > 0) {
-            objc_setAssociatedObject(self, &propertyAryKey, propertyAry, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-            objc_setAssociatedObject(self, &propertyDicKey, propertyDic, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-        }
-        free(property);
-    }
+    //do some setting
+    //    _sessionManager.requestSerializer = [AFJSONRequestSerializer serializer];
+    [_sessionManager.requestSerializer setValue:@"application/json"forHTTPHeaderField:@"Accept"];
+    [_sessionManager.requestSerializer setValue:@"application/json;charset=utf-8"forHTTPHeaderField:@"Content-Type"];
 }
 
-
-#pragma mark - analyticalData
-//---------------analytical---------------
-//data
-- (void)analyticalObjectData:(id)data
+- (void)POST:(NSString *)url CompletionBlcok:(cmpBlock)compBlock
 {
-    if (![data isKindOfClass:[NSDictionary class]]) {
-        [self analyticalAryData:data];
-    }
-    else{
-        [self analyticalDicData:data];
-    }
-}
-
-//ary
-- (void)analyticalAryData:(id)data
-{
-    NSDictionary *propertyDic = objc_getAssociatedObject(self, &propertyDicKey);
-    NSArray *propertyAry = objc_getAssociatedObject(self, &propertyAryKey);
-    if ([propertyAry count] == 1) {
-        NSString *name = [propertyAry lastObject];
-        NSString *attribute = [propertyDic objectForKey:name];
-        NSDictionary *typeDic = [self propertyType:attribute];
-        NSString *typeCode = [typeDic objectForKey:@"typeCode"];
-        Class aryClass = NSClassFromString([typeDic objectForKey:@"className"]);
-        if ([aryClass isSubclassOfClass:[NSArray class]]) {
-            Class protocolClass = NSClassFromString([typeDic objectForKey:@"protocolName"]);
-            if ([protocolClass isSubclassOfClass:[CHModel class]]) {
-                NSArray *modelAry = [self modelAry:data WithClassName:protocolClass];
-                [self setValue:modelAry forKey:name];
-            }
-            else{
-                [self setValue:data forKey:name];
-            }
+    [self.sessionManager POST:url parameters:self.parameters progress:^(NSProgress * _Nonnull uploadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        //set error string
+        //        _errorStr = [responseObject objectForKey:@"key"];
+        //analyze data
+        NSLog(@"%@",responseObject);
+        if (self.parsingStr) {
+            id data = [responseObject objectForKey:self.parsingStr];
+            [self analyzeData:data];
         }else{
-            if ([typeCode isEqualToString:@"@"]) {
-                [self setValue:data forKey:name];
-            }else{
-                SEL sel = NSSelectorFromString([NSString stringWithFormat:@"set%@:",[self upTofirstCharacterIn:name]]);
-                [self setValue:data WithTypeCode:typeCode SEL:sel];
-            }
+            [self analyzeData:responseObject];
         }
+        compBlock(YES);
+        [self releaseSession:self.sessionManager];
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"error %@",error);
+        compBlock(NO);
+        [self releaseSession:self.sessionManager];
+    }];
+}
+
+- (void)GET:(NSString *)url CompletionBlcok:(cmpBlock)compBlock
+{
+    [self.sessionManager GET:url parameters:self.parameters progress:^(NSProgress * _Nonnull uploadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        //set error string
+        //        _errorStr = [responseObject objectForKey:@"key"];
+        //analyze data
+        NSLog(@"%@",responseObject);
+        if (self.parsingStr) {
+            id data = [responseObject objectForKey:self.parsingStr];
+            [self analyzeData:data];
+        }else{
+            [self analyzeData:responseObject];
+        }
+        compBlock(YES);
+        [self releaseSession:self.sessionManager];
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"error %@",error);
+        compBlock(NO);
+        [self releaseSession:self.sessionManager];
+    }];
+}
+
+//完成任务后释放session 不然会造成内存泄漏
+- (void)releaseSession:(AFURLSessionManager *)manager
+{
+    if (manager.session) {
+        [manager.session finishTasksAndInvalidate];
     }
-    
 }
 
 
-//dictionary
-//属性类型： https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/ObjCRuntimeGuide/Articles/ocrtTypeEncodings.html#//apple_ref/doc/uid/TP40008048-CH100-SW1
+#pragma mark - analyze data
 
-- (void)analyticalDicData:(NSDictionary *)data
+- (void)analyzeData:(id)data
 {
-    NSArray *propertyAry = objc_getAssociatedObject(self, &propertyAryKey);
-    NSDictionary *propertyDic = objc_getAssociatedObject(self, &propertyDicKey);
-    
+    if ([data isKindOfClass:[NSArray class]]) {
+        [self analyzeAry:data];
+    }else if ([data isKindOfClass:[NSDictionary class]]){
+        [self analyzeDic:data];
+    }
+}
+
+- (void)analyzeAry:(NSArray *)data
+{
+    NSMutableArray *propertyAry = objc_getAssociatedObject(self, &propertyAryKey);
+    NSMutableArray *propertyDic = objc_getAssociatedObject(self, &propertyDicKey);
     for (NSString *name in propertyAry) {
-        SEL sel = NSSelectorFromString([NSString stringWithFormat:@"set%@:",[self upTofirstCharacterIn:name]]);
-        NSString *attritute = [propertyDic objectForKey:name];
-        NSDictionary *typeDic = [self propertyType:attritute];
-        NSString *typeCode = typeDic[@"typeCode"];
-        NSString *mapperKey = [_mapper objectForKey:name];
-        NSString *finialKey = mapperKey ?: name;
-        id value = [data objectForKey:finialKey];
-        if (![typeCode isEqualToString:@"@"]) {
-            [self setValue:value WithTypeCode:typeCode SEL:sel];
-        }else{
-            NSString *className = typeDic[@"className"];
-            NSString *protocolName = typeDic[@"protocolName"];
-            Class ObjectClass = NSClassFromString(className);
-            if ([ObjectClass isSubclassOfClass:[self class]]) {
-                id object = [[ObjectClass alloc] initWithJson:value];
-                [self setValue:object forKey:name];
+        NSString *attribute = [propertyDic valueForKey:name];
+        NSDictionary *typeDic = [self propertyType:attribute];
+        //        NSString *key = [_mapper objectForKey:name] ?: name;
+        NSString *class = [typeDic valueForKey:className];
+        NSString *protocol = [typeDic valueForKey:protocolName];
+        Class cls = NSClassFromString(class);
+        Class proCls = NSClassFromString(protocol);
+        if ([cls isSubclassOfClass:[NSArray class]] && [proCls isSubclassOfClass:[CHModel class]]) {
+            NSArray *ary = [self analyzeAryFrom:data withClass:proCls];
+            [self setValue:ary forKey:name];
+        }else if ([cls isSubclassOfClass:[NSArray class]]){
+            [self setValue:data forKey:name];
+        }
+    }
+    
+    
+}
+
+- (void)analyzeDic:(NSDictionary *)data
+{
+    NSMutableArray *propertyAry = objc_getAssociatedObject(self, &propertyAryKey);
+    NSMutableArray *propertyDic = objc_getAssociatedObject(self, &propertyDicKey);
+    for (NSString *name in propertyAry) {
+        NSString *attribute = [propertyDic valueForKey:name];
+        NSDictionary *typeDic = [self propertyType:attribute];
+        NSString *key = [_mapper objectForKey:name] ?: name;
+        id value = [data objectForKey:key];
+        NSString *code = [typeDic objectForKey:typeCode];
+        if ([code isEqualToString:@"@"]) {
+            //该属性是oc对象
+            NSString *class = [typeDic objectForKey:className];
+            NSString *protocol = [typeDic objectForKey:protocolName];
+            Class objClass = NSClassFromString(class);
+            Class proClass = NSClassFromString(protocol);
+            if ([objClass isSubclassOfClass:[CHModel class]]) {
+                //是CHModel的子类
+                id obj = [[objClass alloc] initWithJson:value];
+                [self setValue:obj forKey:name];
+            }else if ([objClass isSubclassOfClass:[NSArray class]] && [proClass isSubclassOfClass:[CHModel class]]){
+                //是数组且<>中是CHModel的子类
+                NSArray *ary = [self analyzeAryFrom:(NSArray *)value withClass:proClass];
+                [self setValue:ary forKey:name];
             }else{
-                Class ProtocolClass = NSClassFromString(protocolName);
-                if ([ProtocolClass isSubclassOfClass:[CHModel class]] && [ObjectClass isSubclassOfClass:[NSArray class]]) {
-                    NSArray *ary = [self modelAry:value WithClassName:ProtocolClass];
-                    [self setValue:ary forKey:name];
-                }
-                else{
-                    [self setValue:value forKey:name];
-                }
+                //其他oc对象
+                [self setValue:value forKey:name];
             }
+            
+        }else{
+            //不是oc对象
+            [self setValue:value forKey:name];
         }
     }
 }
 
-- (void)setValue:(id)value WithTypeCode:(NSString *)typeCode SEL:(SEL)sel
+- (NSArray *)analyzeAryFrom:(NSArray *)ary withClass:(Class)Cls
 {
-    if ([typeCode  isEqual: @"i"] || [typeCode  isEqual: @"I"] || [typeCode  isEqual: @"s"] || [typeCode  isEqual: @"S"]) {
-        if ([self respondsToSelector:sel]) {
-            ((void(*)(id,SEL,...))objc_msgSend)(self,sel,[(NSString *)value intValue]);
-        }
+    NSMutableArray *returnAry = [NSMutableArray array];
+    for (id value in ary) {
+        id object = [[Cls alloc] initWithJson:value];
+        [returnAry addObject:object];
     }
-    else if ([typeCode  isEqual: @"q"] || [typeCode  isEqual: @"Q"] || [typeCode  isEqual: @"l"] || [typeCode  isEqual: @"L"])
-    {
-        if ([self respondsToSelector:sel]) {
-            ((void(*)(id,SEL,...))objc_msgSend)(self,sel,[(NSString *)value longLongValue]);
-        }
-    }
-    else if ([typeCode  isEqual: @"f"])
-    {
-        if ([self respondsToSelector:sel]) {
-            ((void(*)(id,SEL,...))objc_msgSend)(self,sel,[(NSString *)value floatValue]);
-        }
-    }
-    else if ([typeCode isEqualToString: @"d"])
-    {
-        if ([self respondsToSelector:sel]) {
-            ((void(*)(id,SEL,...))objc_msgSend)(self,sel,[(NSString *)value doubleValue]);
-        }
-    }
-    else if ([typeCode  isEqual: @"B"])
-    {
-        if ([self respondsToSelector:sel]) {
-            ((void(*)(id,SEL,...))objc_msgSend)(self,sel,[(NSString *)value boolValue]);
-        }
-    }
+    return returnAry;
 }
 
 
-- (NSArray *)modelAry:(NSArray *)ary WithClassName:(Class)cls
-{
-    NSMutableArray *modelAry = [NSMutableArray array];
-    for (id object in ary) {
-        id model = [[cls alloc] initWithJson:object];
-        [modelAry addObject:model];
-    }
-    return modelAry;
-}
-
-
-
-#pragma mark - otherMethod
-//获取类型
+//获取属性的基本信息
 - (NSDictionary *)propertyType:(NSString *)str
 {
-    NSString *typeCode = nil;
-    NSString *className = nil;
-    NSString *protocolName = nil;
-    NSScanner *scanner = [NSScanner scannerWithString:str];
-    [scanner scanUpToString:@"T" intoString:NULL];
-    [scanner scanString:@"T" intoString:NULL];
-    typeCode = [str substringWithRange:NSMakeRange(scanner.scanLocation, 1)];
-    if ([typeCode isEqualToString:@"@"]) {
-        [scanner scanUpToString:@"\"" intoString:NULL];
-        [scanner scanString:@"\"" intoString:NULL];
-        [scanner scanCharactersFromSet:[NSCharacterSet alphanumericCharacterSet] intoString:&className];
-        if ([scanner scanString:@"<" intoString:NULL]) {
-            [scanner scanUpToString:@">" intoString:&protocolName];
+    NSString *code = nil;
+    NSString *class = nil;
+    NSString *protocol = nil;
+    NSScanner *scaner = [NSScanner scannerWithString:str];
+    [scaner scanUpToString:@"T" intoString:nil];
+    [scaner scanString:@"T" intoString:nil];
+    code = [str substringWithRange:NSMakeRange(scaner.scanLocation, 1)];
+    if ([code isEqualToString:@"@"]) {
+        [scaner scanUpToString:@"\"" intoString:NULL];
+        [scaner scanString:@"\"" intoString:NULL];
+        [scaner scanCharactersFromSet:[NSCharacterSet alphanumericCharacterSet] intoString:&class];
+        if ([scaner scanString:@"<" intoString:NULL]) {
+            [scaner scanUpToString:@">" intoString:&protocol];
         }
     }
     NSMutableDictionary *dic = [NSMutableDictionary dictionary];
-    if (typeCode) {
-        [dic setObject:typeCode forKey:@"typeCode"];
+    if (code) {
+        [dic setObject:code forKey:typeCode];
     }
-    if (className) {
-        [dic setObject:className forKey:@"className"];
+    if (class) {
+        [dic setObject:class forKey:className];
     }
-    if (protocolName) {
-        [dic setObject:protocolName forKey:@"protocolName"];
+    if (protocol) {
+        [dic setObject:protocol forKey:protocolName];
     }
-    return dic;
     
+    return dic;
 }
-
-//使首字母大写其他字母不变
-- (NSString *)upTofirstCharacterIn:(NSString *)str
-{
-    NSMutableString *mutableStr = [NSMutableString stringWithString:str];
-    [mutableStr replaceCharactersInRange:NSMakeRange(0, 1) withString:[[str substringToIndex:1] uppercaseString]];
-    return mutableStr;
-}
-
-
 
 @end
